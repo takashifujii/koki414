5e3c1cb122b8f8f2254c1ca152491eccb45f789d
Fujii Takashi <fujii@ficc.jp>
2016-07-26 11:19:14 +0900

    画像のあれ落としたり、アニメーションさせたりとか

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,8 +2,18 @@
 //= require_tree .
 
 $(function(){
+  const $window = $(window);
+
   $(".slider").slick({
     "dots": true,
-    
+    "autoplay": true,
+  });
+
+
+  $window.on("load", () => {
+    setTimeout( () => {
+      $(".catch").addClass('change');
+      $(".brdr").removeClass('st');
+    }, 1000);
   });
 });
