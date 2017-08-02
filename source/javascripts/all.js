6313570ab0495520e34cf416318b75104e015192
Fujii Takashi <fujii@ficc.jp>
2017-08-02 11:53:33 +0900

    なんとなくいろいろ

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,6 +3,14 @@
 $(function(){
   var $window = $(window);
 
+  $(".slider").slick({
+    "dots": false,
+    "autoplay": true,
+    "autoplaySpeed": 2400,
+    "arrows": false,
+    "fade": true
+  });
+
   $window.on("load", function(){
     $(".title span").addClass('change');
   });
